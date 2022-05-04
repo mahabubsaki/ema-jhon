9c02a24ff5fe218921f597e0b6c49445e6a09bfd
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-05-04 21:42:39 +0600

    fixed contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,30 +1,20 @@
 import React, { createContext, useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
-import ContactChild from '../ContactChild/ContactChild';
-export const MyContext = createContext('house')
 
 const Contact = () => {
-    const [num, setNum] = useState(0)
-    const handleOnclick = (number) => {
-        setNum(number + num)
-    }
     return (
-        <MyContext.Provider value={handleOnclick}>
+        <div>
+            <h1>Contact</h1>
+            <Link to="via-facebook">
+                <button>Facebook</button>
+            </Link>
+            <Link to="via-instagram">
+                <button>Instagram</button>
+            </Link>
             <div>
-                <h1>contact</h1>
-                <Link to="via-facebook">
-                    <button>Facebook</button>
-                </Link>
-                <Link to="via-instagram">
-                    <button>Instagram</button>
-                </Link>
-                <div>
-                    <Outlet></Outlet>
-                </div>
-                <h1>{num}</h1>
-                <ContactChild></ContactChild>
+                <Outlet></Outlet>
             </div>
-        </MyContext.Provider>
+        </div>
     );
 };
 
