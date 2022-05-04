1394f4660c9d6371e4befd26ab16c551eca32731
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-05-04 22:30:01 +0600

    hide all firebase config

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAvDohSBesKmEKUCn22GTf0D2bwXSJNEE4",
-    authDomain: "ema-jhon-clone-3c58e.firebaseapp.com",
-    projectId: "ema-jhon-clone-3c58e",
-    storageBucket: "ema-jhon-clone-3c58e.appspot.com",
-    messagingSenderId: "429359415439",
-    appId: "1:429359415439:web:98765ae123edd56128f0fa"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
